accfa5ec18289a6954a3f184b477c5bc84eb3828
kogitant <kogitant@koant.com>
2015-03-11 15:41:40 +0200

    My awesome impl

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,48 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', ['$scope', '$log', function($scope, $log) {
+        $scope.title='Funtodo';
+        $scope.todos = [
+            /*
+             {description:'foo', done:false},
+             {description:'nar', done:false}
+             */
+        ];
 
-}]);
+        $scope.remove = function(todo) {
+            $log.log("Removing: " + angular.toJson(todo));
+            var arrayLength = $scope.todos.length;
+            var t;
+            for (var i = 0; i < arrayLength; i++) {
+                t =  $scope.todos[i];
+                $log.log("Testing: " + angular.toJson(t));
+                if(t.description===todo.description){
+                    $scope.todos.splice(i,1);
+                    break;
+                }
+            }
+        }
+    }])
+    .controller('TodoFormController', ['$scope', '$log', function($scope, $log) {
+        $scope.submit = function() {
+            if ($scope.description) {
+                //$scope.$parent.todos.push( {description:this.description});
+                //$scope.description = '';
+                $log.log($scope.description);
+                $scope.$parent.todos.push({
+                    description:$scope.description,
+                    done:false
+                    });
+                $scope.description = '';
+            }
+        };
+
+    }]);
